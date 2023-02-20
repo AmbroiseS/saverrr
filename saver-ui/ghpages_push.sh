5376342b4c7eaa24c2f38f1a5bbc34c4b1567ad4
Ambroise S <amb.soullier@gmail.com>
2023-02-20 18:24:50 +0100

    [UI][CD] script to deploy to gh pages

--- saver-ui/ghpages_push.sh
+++ saver-ui/ghpages_push.sh
@@ -2,12 +2,13 @@
 set -e
 
 
-ng build --output-path docs --base-href /saver-ui/ --optimization=true --c=production
+ng build --output-path='docs' --base-href='' --optimization=true --c=production
 
 rm -rf ../docs
 mv docs ../docs
+cp ../docs/index.html ../docs/404.html
 
-gir reset
+git reset
 git add ../docs/
 
 git commit -m "[UI] Deploy to github pages"
